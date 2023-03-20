2cb740db715dc8626b16f24c5a71e1655f50a107
Vasyl <vasiliy.vs45@gmail.com>
2023-03-20 06:31:12 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+"use strict" 
+"use script";
+
+// const objRersen = {
+//     name: "John",
+//     age: 37,
+//     isMarried: true
+// };
+
+// console.log(objRersen.age);
+
+
+let incr = 10,
+	dcr = 10
+
+incr++;
+dcr--;
+console.log(incr);
+console.log(dcr);
+
+
+console.log(2+2*2!==8);
