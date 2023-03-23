22855336bb1678631fe31fbf915e4f17389b7106
Vasyl <vasiliy.vs45@gmail.com>
2023-03-23 07:34:30 +0100

    логические операторы

--- js/script.js
+++ js/script.js
@@ -2,21 +2,31 @@
 
 "use strict" 
 
-const numberOfFilms = +prompt ('Сколько фильмов вы уже просмотрели?', '');
-const   personalMovieDB = {
-	count: numberOfFilms,
-	movies: {}, // пустой обьект
-	actors: {}, // пустой обьект
-	genres: [], // пустой массив, где елемнты идут строго по порядку
-	privat: false
-};
+// const numberOfFilms = +prompt ('Сколько фильмов вы уже просмотрели?', '');
+// const   personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {}, // пустой обьект
+// 	actors: {}, // пустой обьект
+// 	genres: [], // пустой массив, где елемнты идут строго по порядку
+// 	privat: false
+// };
 
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-	b = prompt('На сколько вы его оцените?', ''),
-	d = prompt('Один из последних просмотреных фильмов?', ''),
-	c = prompt('На сколько вы его оцените?', '');
+// const a = prompt('Один из последних просмотреных фильмов?', ''),
+// 	  b = prompt('На сколько вы его оцените?', ''),
+// 	  d = prompt('Один из последних просмотреных фильмов?', ''),
+// 	  c = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[d] = c;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[d] = c;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+// var shoppingDone = false;
+
+const hamburger = 3,
+	  fries = 4,
+	  cola =3;
+console.log((hamburger ===3 && fries && cola));
+
+
